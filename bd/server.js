6a5c2fb6a582feb9583cd127541135b5d8296ca1
Jean Rojas <jean.rojas@usach.cl>
2025-02-17 20:43:53 -0300

    Arreglando servidor

--- bd/server.js
+++ bd/server.js
@@ -1,12 +1,11 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT,
     allowExitOnIdle: true
 });
 
