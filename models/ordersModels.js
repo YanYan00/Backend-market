642707b241e4f9969fcc73fc0fbc9f94b7bcb68f
Jean Rojas <jean.rojas@usach.cl>
2025-03-06 10:14:45 -0300

    Arreglando consultas para front

--- models/ordersModels.js
+++ models/ordersModels.js
@@ -1,26 +1,31 @@
 const pool = require('../bd/server.js');
 
-const obtenerPedidosDB = async(id)=>{
+const obtenerComprasDB = async(id) => {
     try {
-        const consulta = 'SELECT d.idDetalle,d.idPedido,d.idProducto,d.cantidad,d.estado,d.precio, p.nombre,p.urlImagen,pe.nombreComprador,pe.direccionComprador FROM detallesPedido d JOIN Pedidos pe ON d.idPedido = pe.idPedido LEFT JOIN Productos p ON d.idProducto = p.idProducto WHERE d.idVendedor=$1';
+        const consulta = 'SELECT d.idDetalle,d.idPedido, d.idProducto,d.cantidad,d.estado,d.precio,p.nombre,p.urlImagen,pe.nombreComprador,pe.emailComprador,u.nombre AS nombreVendedor FROM DetallesPedido d JOIN Pedidos pe ON d.idPedido = pe.idPedido LEFT JOIN Productos p ON d.idProducto = p.idProducto LEFT JOIN Usuarios u ON d.idVendedor = u.idUsuario WHERE pe.idUsuario = $1';
         const values = [id];
-        const result = await pool.query(consulta,values);
+        const result = await pool.query(consulta, values);
+        
+        console.log('Resultados de compras:', result.rows);
         return result.rows;
     } catch (error) {
-        console.error('Error al obtener pedidos:',error);
+        console.error('Error al obtener compras:', error);
         throw error;
-    }
+    } 
 }
-const obtenerComprasDB = async(id)=>{
+
+const obtenerPedidosDB = async(id) => {
     try {
-        const consulta = 'SELECT d.idPedido,d.idProducto,d.cantidad,d.estado,d.precio,p.nombre,p.precio,p.urlImagen FROM detallesPedido d JOIN Pedidos pe ON d.idPedido = pe.idPedido LEFT JOIN Productos p ON d.idProducto = p.idProducto WHERE pe.idUsuario = $1';
+        const consulta = 'SELECT  d.idDetalle, d.idPedido, d.idProducto,  d.cantidad, d.estado, d.precio, p.nombre, p.urlImagen, pe.nombreComprador, pe.direccionComprador, u.nombre AS nombreVendedor FROM DetallesPedido d JOIN Pedidos pe ON d.idPedido = pe.idPedido LEFT JOIN Productos p ON d.idProducto = p.idProducto LEFT JOIN Usuarios u ON pe.idUsuario = u.idUsuario WHERE d.idVendedor = $1';
         const values = [id];
-        const result = await pool.query(consulta,values);
+        const result = await pool.query(consulta, values);
+        
+        console.log('Resultados de pedidos:', result.rows);
         return result.rows;
     } catch (error) {
-        console.error('Error al obtener compras:',error);
+        console.error('Error al obtener pedidos:', error);
         throw error;
-    }
+    } 
 }
 const confirmarEnvioDB = async(id,dato)=>{
     try {
